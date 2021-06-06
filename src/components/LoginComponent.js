59d5623baf60912d0ffb9db21eda5fd341a03430
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-06 18:34:25 -0500

    Added initial version of Login form component.

---
+++ src/components/LoginComponent.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Control, Form, Errors } from 'react-redux-form';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+class Login extends Component {
+    constructor(props) {    
+        super(props);
+        
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }   
+    
+    handleSubmit(values) {
+        this.props.loginUser(values.username, values.password);
+    }
+
+    render() {
+        return(
+            <div className="container">  
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Contact Us</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h3>Welcome back</h3>
+                    </div>
+                    <div className="col-12 col-md-9">
+                          <Form model="loginUser" onSubmit={(values) => this.handleSubmit(values)}>
+                          <Row className="form-group">
+                                <Label htmlFor="username" md={2}>First Name</Label>
+                                <Col md={10}>
+                                    <Control.text model=".username" id="username" name="username"
+                                        placeholder="Username"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".username"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required ',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="password" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Control.text model=".password" id="password" name="password"
+                                        placeholder="Password"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".password"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required ',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{size:10, offset: 2}}>
+                                    <Button type="submit" color="primary">
+                                        Sign in
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Login;
