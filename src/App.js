832553a4b9bf4856f8c0b40a609964701063b648
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-06 16:40:39 -0500

    Added reference to ConfigureStore and fixed reference to Main component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import MainComponent from './components/MainComponent';
+import Main from './components/MainComponent';
 import './App.css';
 import { MemoryRouter } from 'react-router-dom';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component{
-  
   render() {
       return (
         <Provider store={store}>
