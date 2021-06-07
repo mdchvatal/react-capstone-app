acb6228629a8c9acd099f28a6f7ebecc715a932a
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-06 21:41:00 -0500

    Removed login form component (moved it to Header).

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,23 +1,8 @@
 //this should have the login form in a container - see wireframes
 import React, { Component } from 'react';
-import { Jumbotron, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
-import Login from './LoginComponent';
+import { Jumbotron } from 'reactstrap';
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isModalOpen: false
-        };
-        this.toggleModal = this.toggleModal.bind(this);
-    }
-
-    toggleModal() {
-        this.setState({
-            isModalOpen: !this.state.isModalOpen
-        });
-    }
-
     render () {
         return(
             <React.Fragment>
@@ -28,13 +13,6 @@ class Home extends Component {
                                 <h1>LET'S ADD A PICTURE!</h1>
                             </div>
                             <div className="col-3">
-                                <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span>Sign In</Button>
-                                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
-                                    <ModalBody>
-                                        <Login loginUser={this.props.loginUser} resetLoginForm={this.props.resetLoginForm} />
-                                    </ModalBody>
-                                </Modal>
                             </div>
                         </div>    
                     </div>
