2b17074d1aba99b862e2c35783398c4c7af2ea15
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-06 23:38:08 -0500

    Added isStarting property.

--- src/redux/bankingSession.js
+++ src/redux/bankingSession.js
@@ -1,18 +1,20 @@
 import * as ActionTypes from './ActionTypes';
 
 export const BankingSession = (state = {
+        isStarting: false,
         isAuthenticated: false,
-        token: null
+        token: null,
+        errorMessage: null
     }, action) => {
     switch (action.type) {
         case ActionTypes.USER_LOGIN_SUCCEEDED:
-            return {...state, isAuthenticated: true, token: action.payload};
+            return {...state, isStarting: false, isAuthenticated: true, token: action.payload, errorMessage: null};
 
         case ActionTypes.USER_LOGIN_FAILED:
-            return {...state, isAuthenticated: false, token: null};
+            return {...state, isStarting: true, isAuthenticated: false, token: null, errorMessage: action.payload};
 
         case ActionTypes.USER_LOGOUT:
-            return {...state, isAuthenticated: false, token: null};
+            return {...state, isStarting: false, isAuthenticated: false, token: null, errorMessage: null};
     
         default:
             return state;
