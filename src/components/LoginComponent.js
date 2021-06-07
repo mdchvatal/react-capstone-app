a0ccb0cf0f2d55ac17454c171b4d237b714c206c
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-06 21:36:12 -0500

    Cleaned up component.

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Button, Label, Col, Row } from 'reactstrap';
 import { Control, Form, Errors } from 'react-redux-form';
 
 const required = (val) => val && val.length;
@@ -9,8 +8,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
 
 class Login extends Component {
     constructor(props) {    
-        super(props);
-        
+        super(props);        
         this.handleSubmit = this.handleSubmit.bind(this);
     }   
     
@@ -22,78 +20,61 @@ class Login extends Component {
 
     render() {
         return(
-            <div className="container">  
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Contact Us</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>Contact Us</h3>
-                        <hr />
-                    </div>                
-                </div>
-                <div className="row row-content">
-                    <div className="col-12">
-                        <h3>Welcome back</h3>
-                    </div>
-                    <div className="col-12 col-md-9">
-                          <Form model="credentials" onSubmit={(values) => this.handleSubmit(values)}>
-                          <Row className="form-group">
-                                <Label htmlFor="username" md={2}>First Name</Label>
-                                <Col md={10}>
-                                    <Control.text model=".username" id="username" name="username"
-                                        placeholder="Username"
-                                        className="form-control"
-                                        validators={{
-                                            required, minLength: minLength(3), maxLength: maxLength(15)
-                                        }}
-                                    />
-                                    <Errors
-                                        className="text-danger"
-                                        model=".username"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Required ',
-                                            minLength: 'Must be greater than 2 characters',
-                                            maxLength: 'Must be 15 characters or less'
-                                        }}
-                                    />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="password" md={2}>Last Name</Label>
-                                <Col md={10}>
-                                    <Control.text model=".password" id="password" name="password"
-                                        placeholder="Password"
-                                        className="form-control"
-                                        validators={{
-                                            required, minLength: minLength(3), maxLength: maxLength(15)
-                                        }}
-                                    />
-                                    <Errors
-                                        className="text-danger"
-                                        model=".password"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Required ',
-                                            minLength: 'Must be greater than 2 characters',
-                                            maxLength: 'Must be 15 characters or less'
-                                        }}
-                                    />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Col md={{size:10, offset: 2}}>
-                                    <Button type="submit" color="primary">
-                                        Sign in
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Form>
-                    </div>
-                </div>
-            </div>
+            <React.Fragment>
+                <Form model="credentials" onSubmit={(values) => this.handleSubmit(values)}>
+                    <Row className="form-group">
+                        <Label htmlFor="username" md={2}>First Name</Label>
+                        <Col md={10}>
+                            <Control.text model=".username" id="username" name="username"
+                                placeholder="Username"
+                                className="form-control"
+                                validators={{
+                                    required, minLength: minLength(3), maxLength: maxLength(15)
+                                }}
+                            />
+                            <Errors
+                                className="text-danger"
+                                model=".username"
+                                show="touched"
+                                messages={{
+                                    required: 'Required ',
+                                    minLength: 'Must be greater than 2 characters',
+                                    maxLength: 'Must be 15 characters or less'
+                                }}
+                            />
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                        <Label htmlFor="password" md={2}>Last Name</Label>
+                        <Col md={10}>
+                            <Control.text model=".password" id="password" name="password"
+                                placeholder="Password"
+                                className="form-control"
+                                validators={{
+                                    required, minLength: minLength(3), maxLength: maxLength(15)
+                                }}
+                            />
+                            <Errors
+                                className="text-danger"
+                                model=".password"
+                                show="touched"
+                                messages={{
+                                    required: 'Required ',
+                                    minLength: 'Must be greater than 2 characters',
+                                    maxLength: 'Must be 15 characters or less'
+                                }}
+                            />
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                        <Col md={{size:10, offset: 2}}>
+                            <Button type="submit" color="primary">
+                                Sign in
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </React.Fragment>
         );
     }
 
