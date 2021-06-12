499d2ce93f7e5fd8db122b9ad90f17f4d89eda5d
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-12 14:57:01 -0500

    Fixed logo for admin and user authenticated users.

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -82,8 +82,8 @@ class Header extends Component {
                         <Navbar expand="md">
                             <div className="container centering">
                                 <NavbarToggler onClick={this.toggleNav}/>
-                                <NavbarBrand className="mr-auto" href="/home">
-                                    MeritBank <img  alt="nameLogo"/>
+                                <NavbarBrand className="mr-auto" href="/home"> 
+                                    <img alt="nameLogo" src="assets/images/logo.png" alt="Merit Bank" />
                                 </NavbarBrand>
                                 <Collapse isOpen={this.state.isNavOpen} navbar>
                                     <Nav navbar>
@@ -119,8 +119,8 @@ class Header extends Component {
                         <Navbar expand="md">
                             <div className="container centering">
                                 <NavbarToggler onClick={this.toggleNav}/>
-                                <NavbarBrand className="mr-auto" href="/home">
-                                    MeritBank <img  alt="nameLogo"/>
+                                <NavbarBrand className="mr-auto" href="/home"> 
+                                    <img alt="nameLogo" src="assets/images/logo.png" alt="Merit Bank" />
                                 </NavbarBrand>
                                 <Collapse isOpen={this.state.isNavOpen} navbar>
                                     <Nav navbar>
