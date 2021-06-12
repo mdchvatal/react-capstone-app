b48af6d51febd23a49c503a8c32dfd276fa520dc
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-12 13:39:21 -0500

    Updated header to render menu based on role and authentication.

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -49,58 +49,125 @@ class Header extends Component {
     }
 
     render() {
-        return(
-            <React.Fragment>
-                <Navbar  expand="md">
-                    <div className="container centering">
-                        <NavbarToggler onClick={this.toggleNav}/>
-                        <NavbarBrand className="mr-auto" href="/home">
-                            MeritBank <img  alt="nameLogo"/>
-                        </NavbarBrand>
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav navbar>
-                                <UncontrolledDropdown nav inNavbar>
-                                    <DropdownToggle nav caret>Checking</DropdownToggle>
-                                    <DropdownMenu right>
-                                        <DropdownItem tag={Link} to="/checking/merit-checking">Merit Checking</DropdownItem>
-                                        <DropdownItem tag={Link} to="/checking/business-checking">Business Checking</DropdownItem>
-                                    </DropdownMenu>
-                                </UncontrolledDropdown>
-                                <UncontrolledDropdown nav inNavbar>
-                                    <DropdownToggle nav caret>Savings</DropdownToggle>
-                                    <DropdownMenu right>
-                                        <DropdownItem tag={Link} to="/savings/merit-savings">Merit Savings</DropdownItem>
-                                        <DropdownItem tag={Link} to="/savings/business-savings">Business Savings</DropdownItem>
-                                    </DropdownMenu>
-                                </UncontrolledDropdown>
-                                <UncontrolledDropdown nav inNavbar>
-                                    <DropdownToggle nav caret>IRA</DropdownToggle>
-                                    <DropdownMenu right>
-                                        <DropdownItem tag={Link} to="/ira/roth-ira">Roth IRA</DropdownItem>
-                                        <DropdownItem tag={Link} to="/ira/rollover-ira">Rollover IRA</DropdownItem>
-                                        <DropdownItem tag={Link} to="/ira/ira">IRA</DropdownItem>
-                                    </DropdownMenu>
-                                </UncontrolledDropdown>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/cd-accounts">CD</NavLink>
-                                </NavItem>
-                            </Nav>
-                            <Nav className="ml-auto" navbar>
-                                <NavItem>
-                                    <RenderLoginButton isAuthenticated={this.props.bankingSession.isAuthenticated} toggleModal={this.toggleModal} logoutUser={this.props.logoutUser} />
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </div>
-                </Navbar>
-                <Modal isOpen={this.state.isModalOpen && this.props.bankingSession.isStarting} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
-                    <ModalBody>
-                        <Login loginUser={this.props.loginUser} resetLoginForm={this.props.resetLoginForm} bankingSession={this.props.bankingSession} />
-                    </ModalBody>
-                </Modal>
-            </React.Fragment>
-        );
+        if (!this.props.bankingSession || !this.props.bankingSession.isAuthenticated) {
+            return(
+                <React.Fragment>
+                    <Navbar expand="md">
+                        <div className="container centering">
+                            <NavbarToggler onClick={this.toggleNav}/>
+                            <NavbarBrand className="mr-auto" href="/home">
+                                MeritBank <img  alt="nameLogo"/>
+                            </NavbarBrand>
+                            <Collapse isOpen={this.state.isNavOpen} navbar>                            
+                                <Nav className="ml-auto" navbar>
+                                    <NavItem>
+                                        <RenderLoginButton isAuthenticated={this.props.bankingSession.isAuthenticated} toggleModal={this.toggleModal} logoutUser={this.props.logoutUser} />
+                                    </NavItem>
+                                </Nav>
+                            </Collapse>
+                        </div>
+                    </Navbar>
+                    <Modal isOpen={this.state.isModalOpen && this.props.bankingSession.isStarting} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                        <ModalBody>
+                            <Login loginUser={this.props.loginUser} resetLoginForm={this.props.resetLoginForm} bankingSession={this.props.bankingSession} />
+                        </ModalBody>
+                    </Modal>
+                </React.Fragment>
+            );
+        } else if (this.props.bankingSession.isAuthenticated) {
+            if (this.props.bankingSession.role === 'ADMIN') {
+                return(
+                    <React.Fragment>
+                        <Navbar expand="md">
+                            <div className="container centering">
+                                <NavbarToggler onClick={this.toggleNav}/>
+                                <NavbarBrand className="mr-auto" href="/home">
+                                    MeritBank <img  alt="nameLogo"/>
+                                </NavbarBrand>
+                                <Collapse isOpen={this.state.isNavOpen} navbar>
+                                    <Nav navbar>
+                                        <NavItem>
+                                            <NavLink className="nav-link" to="/users">Users</NavLink>
+                                        </NavItem>
+                                        <NavItem>
+                                            <NavLink className="nav-link" to="/cdOfferings">CD Offerings</NavLink>
+                                        </NavItem>
+                                        <NavItem>
+                                            <NavLink className="nav-link" to="/accountHolders">Account Holders</NavLink>
+                                        </NavItem>
+                                    </Nav>
+                                    <Nav className="ml-auto" navbar>
+                                        <NavItem>
+                                            <RenderLoginButton isAuthenticated={this.props.bankingSession.isAuthenticated} toggleModal={this.toggleModal} logoutUser={this.props.logoutUser} />
+                                        </NavItem>
+                                    </Nav>
+                                </Collapse>
+                            </div>
+                        </Navbar>
+                        <Modal isOpen={this.state.isModalOpen && this.props.bankingSession.isStarting} toggle={this.toggleModal}>
+                            <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                            <ModalBody>
+                                <Login loginUser={this.props.loginUser} resetLoginForm={this.props.resetLoginForm} bankingSession={this.props.bankingSession} />
+                            </ModalBody>
+                        </Modal>
+                    </React.Fragment>
+                );
+            } else {
+                return(
+                    <React.Fragment>
+                        <Navbar expand="md">
+                            <div className="container centering">
+                                <NavbarToggler onClick={this.toggleNav}/>
+                                <NavbarBrand className="mr-auto" href="/home">
+                                    MeritBank <img  alt="nameLogo"/>
+                                </NavbarBrand>
+                                <Collapse isOpen={this.state.isNavOpen} navbar>
+                                    <Nav navbar>
+                                        <UncontrolledDropdown nav inNavbar>
+                                            <DropdownToggle nav caret>Checking</DropdownToggle>
+                                            <DropdownMenu right>
+                                                <DropdownItem tag={Link} to="/checking/merit-checking">Merit Checking</DropdownItem>
+                                                <DropdownItem tag={Link} to="/checking/business-checking">Business Checking</DropdownItem>
+                                            </DropdownMenu>
+                                        </UncontrolledDropdown>
+                                        <UncontrolledDropdown nav inNavbar>
+                                            <DropdownToggle nav caret>Savings</DropdownToggle>
+                                            <DropdownMenu right>
+                                                <DropdownItem tag={Link} to="/savings/merit-savings">Merit Savings</DropdownItem>
+                                                <DropdownItem tag={Link} to="/savings/business-savings">Business Savings</DropdownItem>
+                                            </DropdownMenu>
+                                        </UncontrolledDropdown>
+                                        <UncontrolledDropdown nav inNavbar>
+                                            <DropdownToggle nav caret>IRA</DropdownToggle>
+                                            <DropdownMenu right>
+                                                <DropdownItem tag={Link} to="/ira/roth-ira">Roth IRA</DropdownItem>
+                                                <DropdownItem tag={Link} to="/ira/rollover-ira">Rollover IRA</DropdownItem>
+                                                <DropdownItem tag={Link} to="/ira/ira">IRA</DropdownItem>
+                                            </DropdownMenu>
+                                        </UncontrolledDropdown>
+                                        <NavItem>
+                                            <NavLink className="nav-link" to="/cd-accounts">CD</NavLink>
+                                        </NavItem>
+                                    </Nav>
+                                    <Nav className="ml-auto" navbar>
+                                        <NavItem>
+                                            <RenderLoginButton isAuthenticated={this.props.bankingSession.isAuthenticated} toggleModal={this.toggleModal} logoutUser={this.props.logoutUser} />
+                                        </NavItem>
+                                    </Nav>
+                                </Collapse>
+                            </div>
+                        </Navbar>
+                        <Modal isOpen={this.state.isModalOpen && this.props.bankingSession.isStarting} toggle={this.toggleModal}>
+                            <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                            <ModalBody>
+                                <Login loginUser={this.props.loginUser} resetLoginForm={this.props.resetLoginForm} bankingSession={this.props.bankingSession} />
+                            </ModalBody>
+                        </Modal>
+                    </React.Fragment>
+                );
+            }
+        }
     }
 }
 
