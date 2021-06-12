2c8af198f1091586e5b31b022666f6959492b4f8
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-12 13:15:05 -0500

    Stored payload separately.

--- src/redux/bankingSession.js
+++ src/redux/bankingSession.js
@@ -8,13 +8,17 @@ export const BankingSession = (state = {
     }, action) => {
     switch (action.type) {
         case ActionTypes.USER_LOGIN_SUCCEEDED:
-            return {...state, isStarting: false, isAuthenticated: true, token: action.payload, errorMessage: null};
+            if (action.payload != null) {
+                return {...state, isStarting: false, isAuthenticated: true, username: action.payload.username, role: action.payload.role, token: action.payload.jwt, errorMessage: null};
+            } else {
+                return {...state, isStarting: true, isAuthenticated: false, username: null, role: null, token: null, errorMessage: 'Unknown error'};
+            }
 
         case ActionTypes.USER_LOGIN_FAILED:
-            return {...state, isStarting: true, isAuthenticated: false, token: null, errorMessage: action.payload};
+            return {...state, isStarting: true, isAuthenticated: false, username: null, role: null, token: null, errorMessage: action.payload};
 
         case ActionTypes.USER_LOGOUT:
-            return {...state, isStarting: false, isAuthenticated: false, token: null, errorMessage: null};
+            return {...state, isStarting: false, isAuthenticated: false, username: null, role: null, token: null, errorMessage: null};
     
         default:
             return state;
