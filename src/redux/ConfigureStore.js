a2950fb2da02e82849cfc16a431984707a69b8fa
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-12 19:52:43 -0500

    Imported users reducer.

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -1,6 +1,7 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
 import { createForms } from 'react-redux-form';
 import { BankingSession } from './bankingSession';
+import { Users } from './users';
 import { AccountHolderData } from './accountHolderDataReducer'
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
@@ -12,6 +13,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
+            users: Users,
             accountHolderData: AccountHolderData,
             bankingSession: BankingSession,
             ...createForms({
