8a4012edc019390be21de4d1858321a36ff00828
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-13 12:35:48 -0500

    Removed unused imports.

--- src/components/AdminAccountHoldersComponent.js
+++ src/components/AdminAccountHoldersComponent.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, Button, Input, Label, Col, Row } from 'reactstrap';
-import { Control, Form, Errors } from 'react-redux-form';
-import { Link, NavLink, Redirect, withRouter} from 'react-router-dom';
+import { withRouter} from 'react-router-dom';
 
 class AdminAccountHolders extends Component {
     constructor(props) {    
