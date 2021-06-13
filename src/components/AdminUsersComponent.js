373737c46f77ddaa92d9652b1b368aeccaf8f2cc
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-12 19:56:04 -0500

    Added initial loading logic.

--- src/components/AdminUsersComponent.js
+++ src/components/AdminUsersComponent.js
@@ -3,17 +3,56 @@ import { Alert, Button, Input, Label, Col, Row } from 'reactstrap';
 import { Control, Form, Errors } from 'react-redux-form';
 import { Link, NavLink, Redirect, withRouter} from 'react-router-dom';
 
+import { Loading } from './LoadingComponent';
+
 class AdminUsers extends Component {
     constructor(props) {    
-        super(props);
-    }   
+        super(props); 
+    }
+
+    componentDidMount() {
+        if (this.props.status === 'idle') {
+            this.props.fetchUsers(this.props.bankingSession);
+        }
+    }
     
-    render() {        
-        return(
-            <div>
-                Users will go here.
+    render() {
+        console.log('AdminComponent.render');
+        console.log(this.props);
+
+        if (this.props.loading) {
+            return(
+                <div className="container">
+                <div className="row align-items-start">
+                    <h1>Users</h1>
+                    <Loading />
+                </div>
             </div>
-        );
+            )
+        } else {
+            if (this.props.errorMessage) {
+                return(
+                    <div className="container">
+                        <div className="row align-items-start">
+                            <h1>Users</h1>
+                            <Alert color="danger">
+                                {this.props.errorMessage}
+                            </Alert>
+                        </div>
+                    </div>
+                );
+            } else {
+                return(
+                    <div className="container">
+                        <div className="row align-items-start">
+                            <h1>Users</h1>
+                            <div className="col-12 col-md m-1">
+                            </div>
+                        </div>
+                    </div>
+                );    
+            }
+        }
     }
 
 }
